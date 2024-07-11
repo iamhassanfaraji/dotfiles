71b0d464a4cf0e080d28b6648a9f1ac8930f18bc
hassan faraji <iamhassanfarai@gmail.com>
2024-07-11 12:23:11 +0330

    fix unPushed keymap

---
+++ nvim/lua/basics.lua
@@ -0,0 +1,20 @@
+vim.cmd "colorscheme everforest"
+
+vim.keymap.set({"t", "v", "i"}, "<C-Space>", "<C-Bslash><C-n>")
+
+
+------ window managment
+
+
+--- navigation
+vim.keymap.set("n", "wh", "<C-w>h")
+vim.keymap.set("n", "wj", "<C-w>j")
+vim.keymap.set("n", "wk", "<C-w>k")
+vim.keymap.set("n", "wl", "<C-w>l")
+
+
+--- split 
+vim.keymap.set("n", "ws", function() vim.cmd.split("term://bash")end)
+vim.keymap.set("n", "wv", function() vim.cmd.vsplit("term://bash")end)
+
+
