3418b30c900c367dc10e53139969ff4b46d9d095
hassan faraji <iamhassanfarai@gmail.com>
2024-07-27 14:32:03 +0330

    add appearnace

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,12 @@
 local wezterm = require "wezterm"
 local utils = require "utils"
 local pane = require "pane"
+local appearance = require "appearance"
 
 local config = wezterm.config_builder()
 config.keys = utils.flatten_list(pane.keys)
 config.key_tables = utils.merge_all(pane.key_tables)
 config.color_scheme = "Batman"
+config.colors = appearance
 
 return config 
